f80cc84031f80bf2574bc74f683d97172cdf5cfc
Prusotam <prusotamkumaryadav78@gmail.com>
2025-08-24 20:36:56 +0545

    first andriod app build

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isMobile = process.env.NEXT_PUBLIC_IS_MOBILE === 'true';
 const nextConfig = {
+    ...(isMobile ? {output: 'export'} : {}),
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true
-  }
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
